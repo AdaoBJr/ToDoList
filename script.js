412f67bea30ab77f507c7d18b4346b0123cfaf00
Adão Benites <adaobjr@gmail.com>
2021-03-10 17:06:36 -0400

    Desafio 5 - ajuste

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //Cria Novos Itens na Lista de Tarefas
+
 //Adiciona Evento Click no Botão com a Captura da informação do input
 let btnAddItem = document.querySelector('#criar-tarefa');
 btnAddItem.addEventListener('click', insertItemsList);
@@ -7,14 +8,29 @@ function insertItemsList (){
     let textInput = document.querySelector('#texto-tarefa');
     let localOl = document.querySelector('#lista-tarefas');
     
-    if (textInput.value != ' ') {
+    if (textInput.value != '') {
         let listItems = document.createElement('li');
         listItems.innerHTML = textInput.value;
         localOl.appendChild(listItems);
-        textInput.value = ' ';
-    }
-    
+        textInput.value = '';
+    }   
 }
+
+//Adiciona o Background cinza no item clicado
+
+//Adiciona a classe selected no item clicado e retira dos demais
+function removeAddClassSelected (){
+    let itemClass = document.querySelectorAll("li");
+    for (let i = 0; i < colorClass.length; i += 1){
+        colorClass[i].addEventListener("click",removeAddSelected);
+     }
+    function removeAddSelected(){
+        for (let i = 0; i < colorClass.length; i += 1){
+            colorClass[i].classList.remove("selected");
+        }
+        this.classList.add("selected");
+    }
+ }
     
 
 
