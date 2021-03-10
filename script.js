2e6d278de887237441a2c88c9913e783eedaa7df
Adão Benites <adaobjr@gmail.com>
2021-03-10 16:50:11 -0400

    Desafio 5

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//Iniciando
+//Cria Novos Itens na Lista de Tarefas
+//Adiciona Evento Click no Botão com a Captura da informação do input
+let btnAddItem = document.querySelector('#criar-tarefa');
+btnAddItem.addEventListener('click', insertItemsList);
+
+function insertItemsList (){
+    let textInput = document.querySelector('#texto-tarefa');
+    let localOl = document.querySelector('#lista-tarefas');
+    
+    if (textInput.value != ' ') {
+        let listItems = document.createElement('li');
+        listItems.innerHTML = textInput.value;
+        localOl.appendChild(listItems);
+        textInput.value = ' ';
+    }
+    
+}
+    
+
+
+
+
+window.onload = function () {
+    
+};
