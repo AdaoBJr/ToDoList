93e91865698558bb82227682b499776548c3d843
Adão Benites <adaobjr@gmail.com>
2021-03-10 18:23:00 -0400

    Desafio 7

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //Cria Novos Itens na Lista de Tarefas
-
 //Adiciona Evento Click no Botão com a Captura da informação do input
 let btnAddItem = document.querySelector('#criar-tarefa');
 btnAddItem.addEventListener('click', insertItemsList);
@@ -17,25 +16,49 @@ function insertItemsList (){
 }
 
 //Adiciona o Background cinza no item clicado
-
 //Adiciona a classe selected no item clicado e retira dos demais
-function removeAddClassSelected (){
-    let itemClass = document.querySelectorAll("li");
-    for (let i = 0; i < colorClass.length; i += 1){
-        colorClass[i].addEventListener("click",removeAddSelected);
-     }
-    function removeAddSelected(){
-        for (let i = 0; i < colorClass.length; i += 1){
-            colorClass[i].classList.remove("selected");
-        }
-        this.classList.add("selected");
+function removeAddClassSelected (event){
+    let itemClass = document.querySelector('.selected');
+
+    if (!itemClass){
+        event.target.classList.add('selected');
+        event.target.style.cursor = 'pointer';
+    } else {
+        itemClass.classList.remove('selected');
+        event.target.classList.add('selected');
+        event.target.style.cursor = 'pointer';
+    }
+//Podemos fazer esse mesmo algoritmo de cima de uma forma mais didática aberto em passos: (ainda incompleto)
+    // let taskList = document.querySelector('#lista-tarefas');
+    // let tasks = document.querySelectorAll('li');
+    // let qtidLI = taskList.childNodes.length; // definição da quantidade de itens na lista
+    // for (let i = 0; i < qtidLI; i += 1){
+    //    tasks[i].classList.remove('selected');
+    // }
+    //     if (event.target.tagName == 'li'){
+    //         event.target.classList.add('selected')
+    //     }
+
+    
+ }
+ function mouseOver (event){  // Adiciona a propriedade mouseOver (ponteiro indicador do mouse) ao passar o mouse sobre a tarefa
+    let itemClass = document.querySelector('.selected');
+    if (!itemClass){
+        event.target.style.cursor = 'pointer';
+    } else {
+        event.target.style.cursor = 'pointer';
     }
  }
-    
+ 
+function taskSelected () {
+    let taskList = document.querySelector('#lista-tarefas');
+    taskList.addEventListener('click', removeAddClassSelected);
+    taskList.addEventListener('mouseover', mouseOver);
+}
 
 
 
 
 window.onload = function () {
-    
+    taskSelected();
 };
